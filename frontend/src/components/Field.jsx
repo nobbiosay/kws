7ecdc245c2279f2153455cceaeee9bc017b78da1
nobbiosay <nobbiosay@gmail.com>
2025-09-23 09:39:23 +0700

    Delete frontend directory

--- frontend/src/components/Field.jsx
+++
@@ -1,9 +0,0 @@
-export default function Field({label,name,type='number',value,onChange,step='any'}){
-  return (
-    <div>
-      <label className="label" htmlFor={name}>{label}</label>
-      <input id={name} className="input" type={type} value={value ?? ''} step={step}
-        onChange={e=>onChange(name, e.target.value)} />
-    </div>
-  );
-}
