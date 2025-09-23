5f674a09aabc02214ad0ed7e9f912c9a83843d0e
nobbiosay <nobbiosay@gmail.com>
2025-09-23 10:17:41 +0700

    Delete frontend directory

--- frontend/src/services/api.js
+++
@@ -1,4 +0,0 @@
-import axios from 'axios';
-const baseURL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000/api';
-export const api = axios.create({ baseURL });
-export function setToken(t){ api.defaults.headers.common['Authorization'] = `Bearer ${t}`; }
